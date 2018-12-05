148d840e04d064b3b6390198f3372cca37dc9c51
chris-wituchowski <c.wituchowski@gmail.com>
2018-12-04 19:47:07 -0800

    Successful solution to AoC D2P2

--- AdventOfCode2018-2/AdventOfCode2018_2-2.py
+++ AdventOfCode2018-2/AdventOfCode2018_2-2.py
@@ -1,15 +1,275 @@
 import time
 
-inputStr = """abcde
-fghij
-klmno
-pqrst
-fguij
-axcye
-wvxyz"""
+inputStr = """crruafyzloguvxwctqmphenbkd
+srcjafyzlcguvrwctqmphenbkd
+srijafyzlogbpxwctgmphenbkd
+zrijafyzloguvxrctqmphendkd
+srijabyzloguvowcqqmphenbkd
+srijafyzsoguvxwctbmpienbkd
+srirtfyzlognvxwctqmphenbkd
+srijafyzloguvxwctgmphenbmq
+senjafyzloguvxectqmphenbkd
+srijafyeloguvxwwtqmphembkd
+srijafyzlogurxtctqmpkenbkd
+srijafyzlkguvxictqhphenbkd
+srijafgzlogunxwctqophenbkd
+shijabyzloguvxwctqmqhenbkd
+srjoafyzloguvxwctqmphenbwd
+srijafyhloguvxwmtqmphenkkd
+srijadyzlogwvxwctqmphenbed
+brijafyzloguvmwctqmphenhkd
+smijafyzlhguvxwctqmphjnbkd
+sriqafvzloguvxwctqmpheebkd
+srijafyzloguvxwisqmpuenbkd
+mrijakyuloguvxwctqmphenbkd
+srnfafyzloguvxwctqmphgnbkd
+srijadyzloguvxwhfqmphenbkd
+srijafhzloguvxwctdmlhenbkd
+srijafyzloguvxwcsqmphykbkd
+srijafyzlogwvxwatqmphhnbkd
+srijafyzlozqvxwctqmphenbku
+srijafyzloguvxwcbamphenbgd
+srijafyzlfguvxwctqmphzybkd
+srijafyzloguqxwetqmphenkkd
+srijafyylogubxwttqmphenbkd
+srijafyzloguvxzctadphenbkd
+srijafyzloguoxwhtqmchenbkd
+srijafyzloguvxwcvqmzhenbko
+srijnfyzloguvxwctqmchenjkd
+srijaryzloggvxwctqzphenbkd
+srijafhzleguvxwcxqmphenbkd
+ssijafyzllguvxfctqmphenbkd
+srijafyzloguvxdctqmfhenbcd
+srijafyzloguvxfctqmplynbkd
+srijaftzlogavxwcrqmphenbkd
+sriwaoyzloguvxwctqmphenbtd
+srijahyzlogunxwctqmphenbvd
+srjjafyzloguzxwctumphenbkd
+nrijafyzlxguvxwctqmphanbkd
+srijafezlqguyxwctqmphenbkd
+srijafygloguvxwjtqcphenbkd
+erijafyzloguvxoctqmnhenbkd
+ssijafyzllguvxwbtqmphenbkd
+sriaafyzloguvxwctqqphenbkv
+frijafyzloguvswctwmphenbkd
+srijafyzyogkvxwctqmprenbkd
+syijafyzuoguvxwctqmkhenbkd
+srijafyzloganxwctqmphenbkf
+srijafyzloguvxwftqmxhenbkq
+srijafyflogxvxwctqmghenbkd
+srijafyzsoguvxwctqmpjenwkd
+srujafylloguvxwctqmphenckd
+srijafyzlpzuvxwctqmphenbud
+srijafyzlogfvxwctqmhhenbwd
+srijafjzlogusxwctqmphepbkd
+srijlfyzloguvxwctqfphenzkd
+srijafyzlogwvxwctqyphenbqd
+srijafyzloluvxwctqtphenukd
+srizafyzlowuvxwctqmphqnbkd
+sritafkzlkguvxwctqmphenbkd
+sbijafdzloguvxgctqmphenbkd
+crijafyeloguvxwctqmpsenbkd
+srijafyvlogulxwctqmphenbkk
+srijafyologuvxwctqmehegbkd
+siijafyzloguvxwctjmphenbmd
+srijafyzlupuvxwctqmpheabkd
+srijafyzlogumxwctqqphanbkd
+srijxfyzlogujxwcqqmphenbkd
+irijafizeoguvxwctqmphenbkd
+sgijafyzloguvtwctqmpfenbkd
+srijzfyzloguvmwctnmphenbkd
+srijafyzwohuvxwctqmthenbkd
+srijafyzlhguvxoctqwphenbkd
+srgjafyplogxvxwctqmphenbkd
+srijafyqlogovxwctqzphenbkd
+srijafjzloguvlnvtqmphenbkd
+srijafyzooguvxwctqmphenvud
+srijafyzgoguvxwctumphgnbkd
+srijaffzloguvxwdqqmphenbkd
+srijafyzlogugxwctqxphenbkr
+srijafyzlogutxwctqmmcenbkd
+srifafyzlhguwxwctqmphenbkd
+mrimajyzloguvxwctqmphenbkd
+sriyafyzloguvxwcthmphejbkd
+srieakyzlokuvxwctqmphenbkd
+srisafyzloguhxwctqmphecbkd
+srijanyzloguvxcctqmxhenbkd
+srijafyzypguvxwctqmqhenbkd
+sryjtfyzlvguvxwctqmphenbkd
+srijafyzlsguvxwctqmqfenbkd
+srijafyzlogudxwbtqwphenbkd
+srijysyzloguvxwctqmpvenbkd
+srijafyzloggvxwjtqmphegbkd
+srijgfyzloguvxwctqmbhdnbkd
+ssijufyzloguvawctqmphenbkd
+skojafyzloguvxwctqmphenbnd
+srijafylloguvxwcqqmpienbkd
+trioafyzloguvqwctqmphenbkd
+srijafydloguvxwctqmpzjnbkd
+saijafvzloguvxwcqqmphenbkd
+srhjapyzloguvxwctqmbhenbkd
+srijafyzlfguvxwcsqmpwenbkd
+shijafyzboguvxwctqmphenbmd
+srizafysloguvxwrtqmphenbkd
+srijafyzloguvxwciqmwhenbkj
+qrijafyzloduvxwctqmphenbko
+srijefyuloguvxwctqmphenbed
+srijafyzlobuvxwctqmphenhbd
+srijafyzloxuvxwctqmpheabkq
+srijafyzloguvrwctqmghenkkd
+sfisafywloguvxwctqmphenbkd
+srgjafyzlogurxwctqmphenbkp
+srijafhzloguvxwcjqmphenhkd
+srijafyylogufxwrtqmphenbkd
+srijafyzvoguvxwzkqmphenbkd
+sqijafyzloguvxwctqmpheqbxd
+srijafyvloguvxwctqzpherbkd
+srijufyzloguvxlcsqmphenbkd
+srijafykloguvxlccqmphenbkd
+srijafyzloguexwcrqmphenzkd
+sridifyzloguyxwctqmphenbkd
+srijafyzlogfvxwctqlphenbkl
+srijafyzlodqdxwctqmphenbkd
+srijafyzloruvxactqmphenekd
+grijafyzloguvxpctmmphenbkd
+srsjakyzloguvxwctqmphvnbkd
+srikafyvloguvxwrtqmphenbkd
+srijafyzloguvxwctqjpserbkd
+jrijafyzloguvxwctqmpgesbkd
+swijafyzluguvxwctqmfhenbkd
+srijanynlogovxwctqmphenbkd
+jrijafyzloguvxwctymphrnbkd
+srinafyzloguvewctqmphenbzd
+srijakyzloguvxwctqmphcnbka
+srijafyhlobuvxwctqmphenbka
+srijafyzcogusxwctqmphwnbkd
+srijavyzlosuvxwctqmphjnbkd
+orijafyzxoguvxwcnqmphenbkd
+srijafyzlogcvxwvtqmthenbkd
+srijapyzloauvxwctqmphenvkd
+srijaflzloguhxwctqmphenbwd
+smijafyzlonuvxwctqmphenbkw
+jrijafyzloguvxwclqmnhenbkd
+srijaqyzloguvqwctqmphenskd
+srijasyzloguvxwctqmvhenbku
+crijtfyzloguvxwctqmthenbkd
+srrkafyzvoguvxwctqmphenbkd
+srijatyzloguvewctqmphenbld
+srfjafyyloguvnwctqmphenbkd
+srijafyzloguvxwctqjpbenbkt
+hrijafyzooguvxwctqmphenbld
+srijafbzlogscxwctqmphenbkd
+srinafyzlogxvxwctqqphenbkd
+slijafyzloglvxwctqmphenbdd
+srijafyzlogjvxwcsqmphenbld
+sryjcfyzloguvewctqmphenbkd
+srijafyzloguexwctqmohknbkd
+jaijafyzlogevxwctqmphenbkd
+srijafbzlogavxwctqmphenbki
+srijafozlogpvxwctqmphgnbkd
+srijdfyzloguvxwczqmphenbkm
+srijafyzlobuvxwctqmphxndkd
+mrijifyzlhguvxwctqmphenbkd
+srijafyzloguvxbctumphjnbkd
+srijafyzloyuvxwptqmphlnbkd
+arijafyzloguvxwcsqmohenbkd
+srijaftzioguvxwttqmphenbkd
+srijafyzlqsuvxwctqmphxnbkd
+srijafyzioguvxwctqnphetbkd
+prijafbzloguvxdctqmphenbkd
+srijaeyzlnguvxwmtqmphenbkd
+srijofyzloguvqwctqmphonbkd
+srixaryzpoguvxwctqmphenbkd
+srijafyzlowuvxwcwhmphenbkd
+srijafydloguvxwctqmptenikd
+srijqfyzlogtvfwctqmphenbkd
+srijafyzloguvxlctqmpvenbgd
+srijafyzlbguvxwjtqgphenbkd
+srijafyzlohuqxwctqmphenbka
+srijafyzroguvxictqmphynbkd
+srijafyzloguvxdctjmphenjkd
+srijaoczloguvxwctqmphenbjd
+srajafhzloguvxwctqmphenbke
+srijofyzloduvxwctqmphanbkd
+srijafytloguvxwmtnmphenbkd
+srijafyzuoguvxwceqmpgenbkd
+rrijafyzloyuvxwctqmphlnbkd
+srljafyzloguvxictqmohenbkd
+srijafyzlogulxwcrqrphenbkd
+srajafyzloguvxwctqmphanbke
+srijafyzlhguvxwxtqmpheabkd
+sxijafyzloggwxwctqmphenbkd
+srijafyultguvxwctqmphinbkd
+srijafyzloguvtwctqmfhvnbkd
+srijafwzloruvxwctquphenbkd
+srbjafyzxoguuxwctqmphenbkd
+erijafyzlxguvxbctqmphenbkd
+srijagyzlojubxwctqmphenbkd
+srijafyzloguvxwdtqmchenakd
+srijafkzlogukxwctqiphenbkd
+mridafyzloguvxwctqmphenrkd
+szqjafyzloguvxwctqmpheibkd
+srijahyzloguvxwctcmphenekd
+srijafyzloguvxwczpuphenbkd
+srijafyzcoguvfwctqmphenbkq
+qriiafyzloguvxwctqmpheebkd
+srijpfyzloguvxlctqmphenokd
+srijzfyzlotuvxwcjqmphenbkd
+srinafyqloguvxwctfmphenbkd
+srijafyzlogjvxpltqmphenbkd
+srijafyzlotuvxwutqmphenbtd
+sridafyzloguvxwctqmpyenokd
+srxjafyzqogyvxwctqmphenbkd
+ssijafyzzoguvxwctqmphenbad
+srijafrzloguvxwctqmphekpkd
+srijafyzlfgrvxactqmphenbkd
+srijafyzroguvxwttqmphekbkd
+srijefyzloguvxwctqmpqenbrd
+srijefycloguvxwctqmchenbkd
+srzjafyzloguvxwcqqmphanbkd
+srijauyzlhguvxwctqmphenbgd
+srijafyzloguvmwvnqmphenbkd
+srihafyzloguvlwotqmphenbkd
+srigafyzloguvxwctqmphennsd
+sriuafzzloguvxwcuqmphenbkd
+srijavuzllguvxwctqmphenbkd
+srijafjzloguvlnctqmphenbkd
+lrirafyzloguvxwctqmphenbld
+soijarxzloguvxwctqmphenbkd
+srijapyzlnguvxwctqmdhenbkd
+srijafyzkogujxmctqmphenbkd
+srijafuzloguvxwcsqvphenbkd
+srijagyzzoguvxwctqmpvenbkd
+srijafyzlovuvxwctqmrhenbxd
+srijafyzqoguvxwctwmpienbkd
+sxijafyzloguvxwutqmphenlkd
+srijafyzlhgzvxwctqmphqnbkd
+srijajyzloguvxwcbwmphenbkd
+srijazyzloguvxwhtqmphenbkx
+srgjafyzloguvvwctqmphdnbkd
+rrivafyzloguvxjctqmphenbkd
+srijifyzdoguvxwctqmphenbka
+hrijafyzloguvxectqmpheybkd"""
 
 startTime = time.time()
 
 inputList = list(map(str, inputStr.splitlines()))
 
-for string in inputList:
+for string1 in inputList:
+    for string2 in inputList:
+        index = 0
+        comboString = ''
+        if string1 != string2:
+            numNonMatches = 0
+            for char in string1:
+                if char != string2[index]:
+                    numNonMatches += 1
+                else:
+                    comboString += char
+                index += 1
+            if numNonMatches == 1:
+                print('string1: ' + string1 + ' ; string2: ' + string2)
+                print('string without mismatch char: ' + comboString)
+                break
+
+print("%s seconds" % (time.time() - startTime))
